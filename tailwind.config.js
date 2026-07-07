4c74af5a21bc76e2df935cdd7f304ab25bc4ead5
humanstudioacademy <tech@the-human.ai>
2026-07-06 23:09:37 -0300

    feat(theme): add Borna web fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,8 +41,8 @@ const tailwindConfig = {
     extend: {
       fontFamily: {
         sans: defaultSansFonts,
-        inter: ['Inter', ...defaultSansFonts],
-        interDisplay: ['InterDisplay', ...defaultSansFonts],
+        inter: ['Borna', 'Inter', ...defaultSansFonts],
+        interDisplay: ['Borna', 'InterDisplay', ...defaultSansFonts],
       },
       fontWeight: {
         420: '420',
